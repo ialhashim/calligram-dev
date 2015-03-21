00356f5a473f4b73faa4c6089c6f4e698fbac97c
ennetws <ennetws@gmail.com>
2015-03-20 19:47:01 -0700

    Minor.

--- callimaker/js/initialize.js
+++ callimaker/js/initialize.js
@@ -1,9 +1,8 @@
 // Background color
 $("#content").css("background-color", randomBackgroundColor());
 
-// Menu button
+// Menu button animation:
 $("#menu-toggle-button").click(function() {
-
     if(menuState.state === "hidden")
     {
         TweenLite.to("#menu-wrapper", 0.25, {top:"0px", ease: Back.easeOut.config(1)});
@@ -15,3 +14,11 @@ $("#menu-toggle-button").click(function() {
         menuState.state = "hidden";
     }
 });
+
+// Load example shape:
+selectedShape.name = "kangaroo";
+selectedShape.geometry = exampleShape;
+
+$("#selected-word").text(selectedShape.name);
+$("#active-shape").attr("d", selectedShape.geometry );
+
